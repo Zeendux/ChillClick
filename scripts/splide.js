dea4c697a4b2f51cc31b205d60d172f2cb17cf89
zeendux <dima_kuwnir@mail.ru>
2025-09-13 12:56:55 +0300

    slider

--- scripts/splide.js
+++ scripts/splide.js
@@ -1,8 +1,19 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     var splide = new Splide( '.first-splide', {
       type   : 'loop',
+      
     perPage: 3,
+    breakpoints: {
+		640: {
+			perPage: 2,
+      gap: '10px',
+		},
+  },
     autoScroll: { speed: 1 },
+
+    gap: '15px',        // расстояние между ними (0 уберёт зазоры)
+  pagination: false,
+  arrows: false,
     });
     splide.mount(window.splide.Extensions);
     
@@ -15,8 +26,21 @@ document.addEventListener( 'DOMContentLoaded', function() {
   document.addEventListener( 'DOMContentLoaded', function() {
     var splide = new Splide( '.second-splide', {
       type   : 'loop',
+      
     perPage: 3,
+    breakpoints: {
+		640: {
+			perPage: 2,
+      gap: '10px',
+		},
+  },
+    
     autoScroll: { speed: -1 },
+    
+
+    gap: '15px',        // расстояние между ними (0 уберёт зазоры)
+  pagination: false,
+  arrows: false,
     });
     splide.mount(window.splide.Extensions);
     
